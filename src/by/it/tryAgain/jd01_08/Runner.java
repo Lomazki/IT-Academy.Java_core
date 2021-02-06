637ccc96847060a3101bcafaa9bb76933a3ee52c
Lomazki <sedovlasovich@yandex.by>
2021-02-06 19:59:33 +0300

    jd01_08 Scalar finish

--- src/by/it/tryAgain/jd01_08/Runner.java
+++ src/by/it/tryAgain/jd01_08/Runner.java
@@ -1,12 +1,17 @@
 package by.it.tryAgain.jd01_08;
 
 public class Runner {
-    private static void print(Var var) {System.out.println(var);}
+
+    private static void print(Var var) {
+        System.out.println(var);
+    }
     public static void main(String[] args) {
-        Var s = new Scalar(3.0);
+        Var s = new Scalar(3.0); //
         Var v = new Vector(new double[]{1,2,3}); // закомментируйте вектор и/или
         Var m = new Matrix("{{1,2,3},{4,5,6},{7,8,9}}");// матрицу, если вы их не реализовали
         /* Уровень сложности A (калькулятор) */
+//        Scalar sd = new Scalar(15);
+//        print(s.add(sd)); // s - "this", sd - "other" (edited by Sedoy)
         print(s.add(s)); //выведет в консоль 6.0
         print(s.sub(s)); //выведет в консоль 0.0
         print(s.mul(s)); //выведет в консоль 9.0
--- src/by/it/tryAgain/jd01_08/Vector.java
+++ src/by/it/tryAgain/jd01_08/Vector.java
@@ -25,6 +25,26 @@ class Vector extends Var implements Operation{
     }
 
     @Override
+    public Var add(Var other) {
+        return super.add(other);
+    }
+
+    @Override
+    public Var sub(Var other) {
+        return super.sub(other);
+    }
+
+    @Override
+    public Var mul(Var other) {
+        return super.mul(other);
+    }
+
+    @Override
+    public Var div(Var other) {
+        return super.div(other);
+    }
+
+    @Override
     public String toString() {
 
         StringBuilder strBuilder = new StringBuilder("{");
